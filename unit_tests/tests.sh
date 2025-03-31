63c1365874dd75e8a6437be4879407ba78d8631e
PopTurtle <kilian.map@gmail.com>
2025-03-31 17:55:31 +0200

    Ajout d'algorithmes dans les tests unitaires

--- unit_tests/tests.sh
+++ unit_tests/tests.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 codes_dir="./codes/"
-compiler_path="../compiler/parsing"
+compiler_path="../compiler/algosipro"
 compiled_asipro_path="./test_compiled.asipro"
 compiled_sipro_path="./test_compiled.sipro"
+
+RESET='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
 
 #  test_cmd [command] [expected_result] : explicite
 function test_cmd {
         result=$($1)
         if [ $? != 0 ]; then
                 echo ""
-                echo "An error occured during file test"
+                printf "\n${RED}${BOLD}An error occurred during file test${RESET}\n"
                 echo "Command: $1"
                 exit 1
         fi
         if [ $result != $2 ]; then
-                echo "Error on command: $1"
+                printf "\n${RED}${BOLD}Error on command: $1${RESET}\n"
                 echo "Got: $result"
                 echo "Expected: $2"
                 exit -1
@@ -26,6 +31,12 @@ function test_cmd {
 # asipro et sipro (chemins: $compiled_asipro_path et $compiled_sipro_path)
 function compile {
     $compiler_path < $1 > $compiled_asipro_path
+    if [ $? != 0 ]; then
+            echo ""
+            printf "\n${RED}${BOLD}An error occurred during file compilation${RESET}\n"
+            printf "${RED}File: ${1}${RESET}\n"
+            exit 1
+    fi
     asipro $compiled_asipro_path $compiled_sipro_path 2> /dev/null
 }
 
@@ -33,28 +44,35 @@ function compile {
 #    se trouvant au chemin $codes_dir[file_name].algo, et vérifie que le résultat
 #    renvoyé par l'execution est [expected_result].
 function test {
+    echo ""
     echo "Compiling $1.algo"
     compile "$codes_dir$1.algo"
     echo "Testing $1.algo"
     test_cmd "sipro $compiled_sipro_path" $2
-    echo "File $1.algo passed"
-    echo ""
+    printf "${GREEN}${BOLD}>>>\t${RESET}${GREEN}File $1.algo passed${RESET}\n"
 }
 
 # all_tests : Lance les tests unitaires
 function all_tests {
+    printf "\n${GREEN}${BOLD}Starting tests here${RESET}\n"
+
     test simple 5
     test assignement 18
+    test not 30
     test if_and_recursive 13
+    test decr_incr 1115
+    test egyptian_multiplication 1598
     test recursion 81
     test recursion_opt 10
+    test fibonacci 55
+    test mutual_recursion 5460
+    test expr_opt 0
 
-    # fibonacci (10)
-    # mutual recursion
+    printf "\n${GREEN}${BOLD}No error occured during tests${RESET}\n"
 }
 
 
 all_tests
-rm $compiled_asipro_path
-rm $compiled_sipro_path
-rm gmon.out
+rm -f $compiled_asipro_path
+rm -f $compiled_sipro_path
+rm -f gmon.out
